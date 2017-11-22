345ea8318c6074b97f5db409f8917193c0207ddc
IIngaV <inga1996@tut.by>
2017-11-22 22:14:43 +0300

    jscaseupdate

---
+++ js/modules/const.js
@@ -0,0 +1,8 @@
+var box1 = document.getElementById("box1");
+var box2 = document.getElementById("box2");
+var box3 = document.getElementById("box3");
+var box4 = document.getElementById("box4");
+var box5 = document.getElementById("box5");
+var nameSubjects = ["subject1", "subject2", "subject3", "subject4", "subject5", "superSubject"];
+var max = 5;
+var min = 0;
